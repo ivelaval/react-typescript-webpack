f66b511c787b2af6415c28e170c894427189d6cc
Ivan Avila <ivelaval@gmail.com>
2019-10-09 23:07:11 -0500

    New state was added

--- src/app/components/Example.js
+++ src/app/components/Example.js
@@ -2,11 +2,21 @@ import React, { useState } from 'react';
 
 export const InputElement = () => {
   const [inputText, setInputText] = useState('');
+  const [historyList, setHistoryList] = useState([]);
 
   return (
     <div>
-      <input onChange={(e) => { setInputText(e.target.value) }} placeholder="Enter something" /> <br/>
+      <input onChange={(e) => {
+        setInputText(e.target.value);
+        setHistoryList([...historyList, e.target.value])
+      }} placeholder="Enter something" /> <br/>
       entered text: {inputText}
+      <hr/>
+      <ul>
+        {historyList.slice(historyList.length-5).map((history) => {
+          return <li>{history}</li>;
+        })}
+      </ul>
     </div>
   );
 }
